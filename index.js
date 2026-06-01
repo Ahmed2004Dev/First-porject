76166ea37c476c183382d23e643370cab8533bbf
3bmo3tyAhmed2004Dev <amakam207@gmail.com>
2026-06-01 11:27:00 +0300

    Coding Questions - First section (1-5)

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log("hello");
+// console.log("hello");
+
+
+// Coding Questions
+// 1
+// function TransferNumber(num2) {
+//     let num = 123
+//     return String(num+num2)
+// }
+
+// console.log(TransferNumber(7));
+
+// 2
+// function checkNumber(num) {
+//     if (num == 0) {
+//         return "Invalid";
+
+//     } else {
+//         return "Valid";
+//     }
+// }
+
+// console.log(checkNumber(5));
+
+// 3
+// let numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 0; i < numb.length; i++) {
+//     if (numb[i] % 2 == 0) {
+//         continue
+//     } else {
+//         console.log(numb[i]);
+
+//     }
+
+// }
+
+// 4
+// let numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// numb.filter((ah , al , asll)=>{
+// if (ah % 2 == 0) {
+//     console.log(ah);
+// };
+// })
+
+// 5
+// let numb1 = [1, 2, 3, 4, 5];
+// let numb2 = [6, 7, 8, 9, 10];
+// const total = [...numb1 , ...numb2]
+// console.log(total);
