1519b2d159f288f7009cc1b78943e9356cdf1363
3bmo3tyAhmed2004Dev <amakam207@gmail.com>
2026-06-07 09:19:41 +0300

    Bouns Section

---
+++ bouns.js
@@ -0,0 +1,56 @@
+// Example 1
+// var createCounter = function(init) {
+//     let num = init;
+//     function increment() {
+//         return ++num
+//     }
+//     function reset (){
+//        let num = init
+//         return init
+//     }
+//     function decrement() {
+//         return num-2
+//     }
+//     return {increment , reset , decrement}
+// }
+// const counter  = createCounter(7);
+// console.log( counter.increment());
+// console.log( counter.reset());
+// console.log( counter.decrement());
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+
+
+// Eample 2
+// var createCounter = function (init) {
+//     let num = init;
+//     function increment() {
+//         return ++num
+//     }
+//     function increment2() {
+//         return ++num
+//     }
+//     function decrement() {
+//         return num - 1
+//     }
+//     function reset() {
+//         return init
+//     }
+//     function reset2() {
+//         return init
+//     }
+
+//     return { increment, increment2, decrement, reset, reset2 }
+// }
+// const counter = createCounter(5);
+// console.log(counter.increment())
+// console.log(counter.increment2())
+// console.log(counter.decrement())
+// console.log(counter.reset())
+// console.log(counter.reset2())
+
